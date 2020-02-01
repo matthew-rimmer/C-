80497adb13322118b25dc519b99440799cc4de35
Rimmer <matthew.rimmer@protonmail.com>
2020-02-01 15:44:48 +0000

    Initial commit

---
+++ CompilerProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CompilerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = @"x = 5;
+            y = 1;";
+
+            interpreter interp = new interpreter(testString);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CompilerProgram/interpreter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CompilerProgram
+{
+    public class interpreter
+    {
+        private List<string> lines = new List<string>(); //List for containing all lines
+        private Dictionary<string, float> variable = new Dictionary<string, float>(); // 
+
+        public interpreter(string LinesIn)
+        {
+            string newLinesIn = Regex.Replace(LinesIn, @"\s+", "");
+
+            string currentLine = "";
+            
+            for (int i = 0; i < newLinesIn.Length; i++)
+            {
+                if (newLinesIn[i].Equals(';'))
+                {
+                    lines.Add(currentLine);
+                    Console.WriteLine(currentLine);
+                    currentLine = "";
+                }
+                else
+                {
+                    currentLine = currentLine + newLinesIn[i];
+                }
+            }
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                RunLine(lines[i]);
+            }
+        }
+
+        void ProcessAssignment()
+        {
+
+        }
+
+        void RunLine(string line)
+        {
+            //string[] linesPart = line.Split(' ');
+
+            /*Console.WriteLine(linesPart[0]);
+            Console.WriteLine(linesPart[1]);
+            Console.WriteLine(linesPart[2]);*/
+
+
+        }
+
+        void ProcessExpression()
+        {
+
+        }
+    }
+}
