70c2037044061fbc04b6dcc05d3364b7ef3e0c7e
Rimmer <matthew.rimmer@protonmail.com>
2020-02-01 16:53:56 +0000

    Added working REGEX

--- CompilerProgram/interpreter.cs
+++ CompilerProgram/interpreter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -8,20 +9,22 @@ namespace CompilerProgram
     public class interpreter
     {
         private List<string> lines = new List<string>(); //List for containing all lines
-        private Dictionary<string, float> variable = new Dictionary<string, float>(); // 
+        private Dictionary<string, float> variable = new Dictionary<string, float>(); // Dictionary for variables
 
         public interpreter(string LinesIn)
         {
             string newLinesIn = Regex.Replace(LinesIn, @"\s+", "");
 
             string currentLine = "";
-            
+
+            //Console.WriteLine(newLinesIn);
+
             for (int i = 0; i < newLinesIn.Length; i++)
             {
                 if (newLinesIn[i].Equals(';'))
                 {
                     lines.Add(currentLine);
-                    Console.WriteLine(currentLine);
+                    //Console.WriteLine(currentLine);
                     currentLine = "";
                 }
                 else
@@ -43,8 +46,27 @@ namespace CompilerProgram
 
         void RunLine(string line)
         {
-            //string[] linesPart = line.Split(' ');
+            //Console.WriteLine(line);
+            string[] parts = Regex.Split(line, @"(\d+(,\d+)*(?:.\d+)?(?:[eE][-+]?[0-9]+)?|[-^+/()]|\w+)").Where(s => s != String.Empty).ToArray<string>(); ;
 
+            Console.WriteLine(parts[0]);
+            Console.WriteLine(parts[1]);
+            Console.WriteLine(parts[2]);
+            /*List<string> parts = new List<string>();
+            
+            
+            
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (Regex.IsMatch(line[i].ToString(), @"\d"))
+                {
+
+                }
+            }
+            */
+
+
+            //string[] linesPart = line.Split(' ');
             /*Console.WriteLine(linesPart[0]);
             Console.WriteLine(linesPart[1]);
             Console.WriteLine(linesPart[2]);*/
